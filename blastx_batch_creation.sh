014a17acb9a4ea5fef0b1456744e56b095a5445d
makacom <60013711+makacom@users.noreply.github.com>
2020-02-05 11:47:04 -0500

    Update and rename blastx_batchcreation.sh to blastx_batch_creation.sh

--- blastx_batch_creation.sh
+++ blastx_batch_creation.sh
@@ -1,5 +1,5 @@
 echo "#!/bin/sh
-#SBATCH --account=def-germain1
+#SBATCH --account=$ACCOUNT
 #SBATCH --job-name=blast
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
@@ -11,14 +11,13 @@ module load nixpkgs/16.09 gcc/7.3.0
 module load blast+
 
 echo 'Starting blastx'
-cd /scratch/karencgs/INRA/assembly_larch/trinity_larch/Larch_annotation/trinotate/batch_cds/
-blastx -query $1\
- -db /cvmfs/soft.mugqic/CentOS6/genomes/blast_db/uniprot_sprot.trinotate_v2.0.pep\
+cd $2
+blastx -query $3\
+ -db $4\
  -evalue 1e-5\
  -task blastx\
  -num_threads 16\
  -outfmt 6\
- > blastx_transdecoder.outfmt6_$2
+ > blastx_transdecoder.outfmt6_$5
 
 echo 'Finished blastx' "
-~
